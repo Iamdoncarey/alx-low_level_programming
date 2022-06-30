aaeb14f7a15926ca2a93fb9a97e4f579b5b30835
iamdoncarey <careyfynface@gmail.com>
2022-06-30 15:14:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *   * main - Prints "Programming is like building a multilingual puzzle
+ *
+ *     *
+ *
+ *       * Return: Zero(0) for Success
+ *
+ *         */
+
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
