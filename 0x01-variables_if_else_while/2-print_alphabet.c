1b5bdc538c9a948c2b0faf619f0a544ccda2a90e
iamdoncarey <careyfynface@gmail.com>
2022-07-02 03:15:51 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point for C code
+ *
+ * Return: Zero(0) for Sucess. The endpoint for c code
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
