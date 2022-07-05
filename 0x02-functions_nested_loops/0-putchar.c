e345656b2f4ce6a333e7b3849360af8242a92217
iamdoncarey <careyfynface@gmail.com>
2022-07-04 23:48:43 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for C code.
+ *
+ * Return: Zero(0), Success Exit Status.
+ */
+int main(void)
+
+{
+
+	_putchar('_');
+
+	_putchar('p');
+
+	_putchar('u');
+
+	_putchar('t');
+
+	_putchar('c');
+
+	_putchar('h');
+
+	_putchar('a');
+
+	_putchar('r');
+
+	_putchar('\n');
+
+	return (0);
+
+}
