fb67d7c15fba4c4b157cafde8c2518dc4313ad31
iamdoncarey <careyfynface@gmail.com>
2022-07-12 14:23:07 +0100

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string in reverse, followed by a new line, to stdout.
+  * @str: Pointer to string.
+  *
+  */
+ void print_rev(char *s);
+ {
+ 	int i, count = 0;
+ 	
+ 	for (i = 0; s[i] != '\0'; i++)
+ 	{
+ 	count ++;
+ 	}
+	for  (i + count - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');	
+}
