477bdf90fcb01529591f2ce96bc789f52c25a78a
iamdoncarey <careyfynface@gmail.com>
2022-07-21 08:11:07 +0100

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,15 +3,15 @@
 /**
   * _puts_recursion - calls string s recursively.
   * @s: string.
+  *
   */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+	else
+		_putchar('\n');
 }
