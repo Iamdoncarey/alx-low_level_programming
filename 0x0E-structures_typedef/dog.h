47329df698b954f2163ef82e6ca24601d060d59d
iamdoncarey <careyfynface@gmail.com>
2022-08-02 08:05:47 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,24 @@
-#ifndef _DOG_H
-#define _DOG_H
+#ifndef STRTYP
+#define STRTYP
 
-#include "my_dog.h"
+/**
+* struct dog - structure of a dog.
+* @name: name of dog.
+* @age: age of dog.
+* @owner: owner of dog.
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-#include "dog_t.h"
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* _DOG_H */
+#endif /*STRTYP*/
